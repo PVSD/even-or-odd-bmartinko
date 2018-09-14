352976618d118dcae990e56109296ae4434e0262
bmartinko <bennett.martinko@gmail.com>
2018-09-13 23:40:15 -0400

    Shout out to Todd for the cool idea to use a static boolean variable. It made making the program a lot easier, and was very simple to implement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
+import java.util.*;
 
 public class Main {
-
+public static boolean evenOrOdd(int number){
+    if (number % 2 == 0){
+        return true;
+    }else {
+        return false;
+    }
+}
     public static void main(String[] args) {
-	// write your code here
+Scanner kbScanner = new Scanner(System.in);
+        System.out.println("Type in an integer:");
+        int num = kbScanner.nextInt();
+
+        if (evenOrOdd(num)){
+            System.out.println("The number " + num + " is even.");
+        }else {
+            System.out.println("The number " + num + " is odd.");
+        }
     }
 }
